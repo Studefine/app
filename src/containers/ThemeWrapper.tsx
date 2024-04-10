9bcde74953eb728d4f5d4c6d3885b0030f390306
gabor <gabor.d.nemeth@gmail.com>
2024-04-10 13:17:51 +0200

    Refactor: create folder structure
    This change create a folder structure, and add some dummy files into these folders.
    Some existing files are organizing.

---
+++ src/containers/ThemeWrapper.tsx
@@ -0,0 +1,11 @@
+import React, {PropsWithChildren} from 'react';
+
+const ThemeWrapper: React.FC<PropsWithChildren> = ({children}) => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default ThemeWrapper;
