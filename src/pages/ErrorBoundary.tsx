bdf0691e28cfd97dc5e22257a17567bca226d116
gabor <gabor.d.nemeth@gmail.com>
2024-04-11 00:28:42 +0200

    Style: ErrorBoundary.tsx

--- src/pages/ErrorBoundary.tsx
+++ src/pages/ErrorBoundary.tsx
@@ -1,8 +1,9 @@
 import React from "react";
 import Text from "../components/Text";
-
+import "../index.css";
+// todo fix this page to look nice
 const ErrorBoundary = () => {
-  return <Text>Hmm, nem gépeltél el valamit?</Text>;
+  return <Text fontSize={"lg"} color={"error"}>Hmm, nem gépeltél el valamit?</Text>;
 };
 
 export default ErrorBoundary;
